afd9ca202874eeec28e1add1db8bd8ed611ce012
docAlexxx <docalex@mail.ru>
2021-12-28 00:05:43 +0400

    lesson2: added viewBinding

--- app/src/main/java/com/example/weather/view/MainActivity.kt
+++ app/src/main/java/com/example/weather/view/MainActivity.kt
@@ -3,11 +3,17 @@ package com.example.weather.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.weather.R
+import com.example.weather.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
     }
 }
