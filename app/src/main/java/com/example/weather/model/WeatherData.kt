32c48c16d8a3a2d9f49474245c42a537ba63d931
docAlexxx <docalex@mail.ru>
2021-12-31 00:21:34 +0400

    lesson3: set Parselize

--- app/src/main/java/com/example/weather/model/WeatherData.kt
+++ app/src/main/java/com/example/weather/model/WeatherData.kt
@@ -1,11 +1,16 @@
 package com.example.weather.model
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class WeatherData(
     val city: City = getDefaultCity(),
     val temperature: Int = -20,
     val feelsLike: Int = -25
-)
+) : Parcelable
 
+@Parcelize
 data class City(val name: String, val lon: Double, val lat: Double)
 
 fun getDefaultCity() = City("Samara", 53.2001, 50.15)
