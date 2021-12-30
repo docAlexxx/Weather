e112c39c97908a4442fec443b3357d0fe4331e54
docAlexxx <docalex@mail.ru>
2021-12-31 00:32:18 +0400

    lesson3: edit CityFragment

--- app/src/main/java/com/example/weather/view/CityFragment.kt
+++ app/src/main/java/com/example/weather/view/CityFragment.kt
@@ -8,10 +8,12 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.weather.databinding.FragmentCityBinding
+import com.example.weather.model.WeatherData
 import com.example.weather.viewmodel.AppStatement
 import com.example.weather.viewmodel.MainViewModel
 import com.google.android.material.snackbar.Snackbar
 
+const val BUNDLE_KEY = "key"
 
 class CityFragment : Fragment() {
     var _binding: FragmentCityBinding? = null
@@ -35,50 +37,29 @@ class CityFragment : Fragment() {
     }
 
     companion object {
-        fun newInstance() = CityFragment()
+        fun newInstance(bundle:Bundle):CityFragment {
+            val fragment  = CityFragment()
+            fragment.arguments = bundle
+            return fragment
+        }
     }
 
     private lateinit var viewModel: MainViewModel
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        viewModel.getLiveData()
-            .observe(viewLifecycleOwner, Observer<AppStatement> { checkData(it) })
-      //  viewModel.getWeather()
-    }
-
-    fun checkData(appState: AppStatement) {
-        when (appState) {
-            is AppStatement.Error -> {
-                binding.loadingLayout.visibility = View.GONE
-                Snackbar.make(binding.mainView, "Can't load data", Snackbar.LENGTH_LONG)
-                    .setAction("Try again") {
-                      //  viewModel.getWeather()
-                    }.show()
-            }
-            is AppStatement.Loading -> {
-                binding.loadingLayout.visibility = View.VISIBLE
-            }
-            is AppStatement.Success -> {
-                binding.loadingLayout.visibility = View.GONE
-                //  Snackbar.make(binding.mainView,"${appState.temperature} (${appState.feelsLike} )",Snackbar.LENGTH_LONG).show()
-
-             /*   binding.cityName.text = appState.weatherData.city.name
-                binding.cityCoordinates.text =
-                    "${appState.weatherData.city.lat}, ${appState.weatherData.city.lon}"
-                binding.temperatureValue.text = "${appState.weatherData.temperature}"
-                binding.feelsLikeValue.text = "${appState.weatherData.feelsLike}"*/
-
-                Snackbar.make(
-                    binding.mainView,
-                    "Success",
-                    Snackbar.LENGTH_LONG
-                ).show()
-
-            }
+        val weather = arguments?.getParcelable<WeatherData>(BUNDLE_KEY)
+        if(weather!=null){
+            setWeatherData(weather)
         }
-
-
     }
+
+    private fun setWeatherData(weather: WeatherData) {
+        binding.cityName.text = weather.city.name
+        binding.cityCoordinates.text =
+            "${weather.city.lat}, ${weather.city.lon}"
+        binding.temperatureValue.text = "${weather.temperature}"
+        binding.feelsLikeValue.text = "${weather.feelsLike}"
+    }
+
 }
