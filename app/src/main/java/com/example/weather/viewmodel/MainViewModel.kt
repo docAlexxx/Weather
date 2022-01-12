2ded8bbb251a24210729436801565d793e058778
docAlexxx <docalex@mail.ru>
2022-01-12 20:06:22 +0400

    lesson4: MainViewModel corrected

--- app/src/main/java/com/example/weather/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/weather/viewmodel/MainViewModel.kt
@@ -1,6 +1,5 @@
 package com.example.weather.viewmodel
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.weather.model.RepoImpl
@@ -8,12 +7,12 @@ import java.lang.Thread.sleep
 
 class MainViewModel(
     private val liveData: MutableLiveData<AppStatement> = MutableLiveData(),
-    private val repoImpl: RepoImpl = RepoImpl()
 ) : ViewModel() {
-
-    fun getLiveData(): LiveData<AppStatement> {
-        return liveData
+    private val repoImpl: RepoImpl by lazy {
+        RepoImpl()
     }
+
+    fun getLiveData() = liveData
 
     fun getWeatherFromLocalSourceRus() = getWeather(true)
 
@@ -24,17 +23,19 @@ class MainViewModel(
     fun getWeather(isRussian: Boolean) {
         liveData.postValue(AppStatement.Loading(0))
         Thread {
-            sleep(1000)
+            sleep(500)
             val randomResult = (1..100).random()
             if (randomResult > 90) {
                 liveData.postValue(AppStatement.Error(IllegalStateException("")))
             } else {
                 liveData.postValue(
                     AppStatement.Success(
-                        if (isRussian) {
-                            repoImpl.getWeatherFromLocalStorageRus()
-                        } else {
-                            repoImpl.getWeatherFromLocalStorageWorld()
+                        repoImpl.let {
+                            if (isRussian) {
+                                it.getWeatherFromLocalStorageRus()
+                            } else {
+                                it.getWeatherFromLocalStorageWorld()
+                            }
                         }
                     )
                 )
