76707f30a327d3ae737425cb04973647561532dd
docAlexxx <docalex@mail.ru>
2022-01-28 19:39:24 +0400

    lesson8: added HistoryFragment and ViewModel for it

---
+++ app/src/main/java/com/example/weather/view/history/HistoryFragment.kt
@@ -0,0 +1,84 @@
+package com.example.weather.view.history
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.weather.R
+import com.example.weather.databinding.FragmentHistoryBinding
+import com.example.weather.model.WeatherData
+import com.example.weather.view.list.ListFragmentAdapter
+import com.example.weather.view.list.OnItemClick
+import com.example.weather.viewmodel.AppStatement
+import com.example.weather.viewmodel.HistoryViewModel
+import com.google.android.material.snackbar.Snackbar
+
+class HistoryFragment  : Fragment(), OnItemClick {
+
+    private var _binding: FragmentHistoryBinding? = null
+    private val binding: FragmentHistoryBinding
+        get() {
+            return _binding!!
+        }
+
+    private val adapter: ListFragmentAdapter by lazy {
+        ListFragmentAdapter(this)
+    }
+
+    private val viewModel: HistoryViewModel by lazy {
+        ViewModelProvider(this).get(HistoryViewModel::class.java)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.getLiveData().observe(viewLifecycleOwner, Observer<AppStatement> { renderData(it) })
+        viewModel.getAllHistory()
+        binding.historyFragmentRecyclerview.adapter = adapter
+    }
+
+
+    private fun renderData(appState: AppStatement) {
+        with(binding) {
+            when (appState) {
+                is AppStatement.Error -> {
+                    binding.root.showSnackBarWithoutAction(
+                               getString(R.string.error_history_load_text),
+                               Snackbar.LENGTH_SHORT
+                           )
+                }
+                is AppStatement.Loading -> {}
+                is AppStatement.Success -> {
+                    adapter.setWeather(appState.weatherData)
+                }
+            }
+        }
+    }
+
+    private fun View.showSnackBarWithoutAction(text: String, long: Int) {
+        Snackbar.make(this, text, long).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHistoryBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    companion object {
+        fun newInstance() = HistoryFragment()
+    }
+
+    override fun onItemClick(weather: WeatherData) {
+
+    }
+}
---
+++ app/src/main/java/com/example/weather/viewmodel/HistoryViewModel.kt
@@ -0,0 +1,24 @@
+package com.example.weather.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.weather.repo.RepoHistoryWeatherImpl
+
+class HistoryViewModel(
+    private val liveData: MutableLiveData<AppStatement> = MutableLiveData(),
+) : ViewModel() {
+
+    private val repoHistoryWeatherImpl: RepoHistoryWeatherImpl by lazy {
+        RepoHistoryWeatherImpl()
+    }
+
+    fun getLiveData() = liveData
+
+    fun getAllHistory() {
+        val listWeather = repoHistoryWeatherImpl.getAllHistoryWeather()
+        liveData.postValue(AppStatement.Success(listWeather))
+
+    }
+
+
+}
